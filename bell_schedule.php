e4d52afd547649e6d3c111b57349505f85f4227b
Chantelle <rasred7@gmail.com>
2019-05-06 10:34:19 -0400

    took out dropdown menu

--- bell_schedule.php
+++ bell_schedule.php
@@ -88,14 +88,6 @@
         <a href="bell_schedule.php">Different Bell Schedules</a>
         <a href="displaybellschedule.php">Today's Bell Schedule</a>
         <a href="day_selector.php">Submit a Schedule Change</a>
-        <div class="dropdown">
-        <button class="dropbtn" onclick="myFunction()">Dropdown
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-content" id="myDropdown">
-          <a href="http://www.bloomingtonsouth.org/plus/login.php">Panther Plus</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
         </div>
         </div>
       </div>
