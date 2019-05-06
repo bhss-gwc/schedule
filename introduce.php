6e0564768cad0089f4d1140f12740893ebced262
Chantelle <rasred7@gmail.com>
2019-05-06 10:39:14 -0400

    took out dropdown menu

--- introduce.php
+++ introduce.php
@@ -87,14 +87,6 @@
 				<a href="bell_schedule.php">Different Bell Schedules</a>
 	      <a href="displaybellschedule.php">Today's Bell Schedule</a>
 				<a href="day_selector.php">Submit a Schedule Change</a>
-	      <div class="dropdown">
-	      <button class="dropbtn" onclick="myFunction()">Dropdown
-	        <i class="fa fa-caret-down"></i>
-	      </button>
-	      <div class="dropdown-content" id="myDropdown">
-	        <a href="#">Link 1</a>
-	        <a href="#">Link 2</a>
-	        <a href="#">Link 3</a>
 	      </div>
 	      </div>
 	    </div>
