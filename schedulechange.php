3b9dcb5ed9b6d89ba6f6b64a3cc2a448e7a89934
Elenna Kim <kimele2003@gmail.com>
2019-05-13 10:44:02 -0400

    added month and day dropdown

--- schedulechange.php
+++ schedulechange.php
@@ -100,7 +100,59 @@
 
 <h1>Form: Schedule Changes</h1>
 <form action = "handlerschedulechange.php" method="POST">
-Date of schedule change: <br>
+
+  Select the month and day you are setting the schedule for:
+  <form action = "handlerday_selector.php" method="POST">
+
+  <?php
+    echo '<SELECT name = "month">';
+    if($currentmonth <= 8 and $currentmonth > 5){
+      echo '<option value = "08">Aug</option>';
+    }
+    if($currentmonth <= 9 and $currentmonth > 5){
+      echo '<option value = "09">Sep</option>';
+    }
+    if($currentmonth <= 10 and $currentmonth > 5){
+      echo '<option value = "10">Oct</option>';
+    }
+    if($currentmonth <= 11 and $currentmonth > 5){
+      echo '<option value = "11">Nov</option>';
+    }
+    if($currentmonth <= 12 and $currentmonth > 5){
+      echo '<option value = "12">Dec</option>';
+    }
+    if($currentmonth <= 1){
+      echo '<option value = "01">Jan</option>';
+    }
+    if($currentmonth <= 2){
+      echo '<option value = "02">Feb</option>';
+    }
+    if($currentmonth <= 3){
+      echo '<option value = "03">Mar</option>';
+    }
+    if($currentmonth <= 4){
+      echo '<option value = "04">Apr</option>';
+    }
+    if($currentmonth <= 5){
+      echo '<option value = "05">May</option>';
+    }
+
+
+  echo '</select>';
+
+  echo '<SELECT name = "date">';
+    for ($i = 1; $i < 32; $i++){
+      echo '<option value = "' . $i . '">' . $i . '</option>';
+    }
+  echo '</select>';
+
+
+  echo '<p>
+    Choose which schedule to apply for that day:
+  </p>';
+
+  ?>
+
 <input type = "text" name = "dateofschedulechange"/><br><br>
 1st period: (example: 8-8:50 am)<br>
 <input type = "text" name = "1stperiod"/><br><br>
