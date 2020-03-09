41bb407abb2279fff44bd792895a2fdca73da053
Elenna Kim <kimele2003@gmail.com>
2020-03-09 10:57:41 -0400

    Removed day selector drop down

--- submitnewschedule.php
+++ submitnewschedule.php
@@ -14,57 +14,11 @@ $admin_username = "admin";
 <h2>Submit a New Schedule</h2>
 This form is for creating an entirely new bell schedule for a certain day.<br><br><br>
 
-  1. Select the month and day you are setting the schedule for:<br><br>
-  <form action = "handlersubmitnewschedule.php" method="POST">
 
-  <?php
-  echo '<SELECT name = "month">';
-  if($currentmonth <= 8 and $currentmonth > 5){
-    echo '<option value = "08">Aug</option>';
-  }
-  if($currentmonth <= 9 and $currentmonth > 5){
-    echo '<option value = "09">Sep</option>';
-  }
-  if($currentmonth <= 10 and $currentmonth > 5){
-    echo '<option value = "10">Oct</option>';
-  }
-  if($currentmonth <= 11 and $currentmonth > 5){
-    echo '<option value = "11">Nov</option>';
-  }
-  if($currentmonth <= 12 and $currentmonth > 5){
-    echo '<option value = "12">Dec</option>';
-  }
-  if($currentmonth <= 1){
-    echo '<option value = "01">Jan</option>';
-  }
-  if($currentmonth <= 2){
-    echo '<option value = "02">Feb</option>';
-  }
-  if($currentmonth <= 3){
-    echo '<option value = "03">Mar</option>';
-  }
-  if($currentmonth <= 4){
-    echo '<option value = "04">Apr</option>';
-  }
-  if($currentmonth <= 5){
-    echo '<option value = "05">May</option>';
-  }
-
-
-echo '</select>';
-
-echo '<SELECT name = "date">';
-  for ($i = 1; $i < 32; $i++){
-    echo '<option value = "' . $i . '">' . $i . '</option>';
-  }
-echo '</select>';
-
-  ?>
-
-<br><br>2. Name of new schedule type (ex: ISTEP math part 1):<br><br>
+<br><br>1. Name of new schedule type (ex: ISTEP math part 1):<br><br>
 <input type = "text" name = "newscheduletype"/><br>
 
-<br>3. Insert bell schedule times:<br><br>
+<br>2. Insert bell schedule times:<br><br>
 1st period:
 <input type = "time" name = "1stperiodstart"/> - <input type = "time" name = "1stperiodstop"/><br><br>
 2nd period:
